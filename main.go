d5d7599c5a71eeaa51f2deed4487347fbb670d7e
Abado Jack Mtulla <abadojack@gmail.com>
2019-01-26 12:24:43 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/abadojack/rest-api/handlers"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/people", handlers.PersonsHandler).Methods("GET")
+	router.HandleFunc("/people/{id}", handlers.PersonHandler).Methods("GET")
+	router.HandleFunc("/people/{id}", handlers.CreatePersonHandler).Methods("POST")
+	router.HandleFunc("/people/{id}", handlers.DeletePersonHandler).Methods("DELETE")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
